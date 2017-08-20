ba12fba640caa58b0b468b5430aab7803ab60383
alexayan <773655223@qq.com>
2017-08-20 13:23:15 +0800

    bug fix

--- src/render.js
+++ src/render.js
@@ -8,16 +8,16 @@ import { renderToStaticMarkup } from 'react-dom/server';
 export default async function render({ url, env, routes, renderFullPage, createApp, initialState, onRenderSuccess }) {
   initialState = initialState || {};
   return new Promise((resolve, reject) => {
-    try {
-      match({
-        routes,
-        location: url,
-      }, async (err, redirectLocation, renderProps) => {
-        if (err) {
-          resolve({ code: 500, url, env, error: err });
-        } else if (redirectLocation) {
-          resolve({ code: 302, url, env, redirect: redirectLocation.pathname + redirectLocation.search });
-        } else if (renderProps) {
+    match({
+      routes,
+      location: url,
+    }, async (err, redirectLocation, renderProps) => {
+      if (err) {
+        resolve({ code: 500, url, env, error: err });
+      } else if (redirectLocation) {
+        resolve({ code: 302, url, env, redirect: redirectLocation.pathname + redirectLocation.search });
+      } else if (renderProps) {
+        try {
           const pathname = renderProps.location.pathname;
           const state = merge({}, initialState, {
             app: {
@@ -30,14 +30,15 @@ export default async function render({ url, env, routes, renderFullPage, createA
             await onRenderSuccess({ html, url, env, state })
           }
           resolve({ code: 200, url, env, html });
-        } else {
-          resolve({ code: 404, url, env });
+        } catch (e) {
+          console.error(e);
+          resolve({ code: 500, url, env, error: err });
         }
-      });
-    } catch (err) {
-      console.error({ code: 500, url, env, error: err });
-      resolve({ code: 500, url, env, error: err });
-    }
+      } else {
+        resolve({ code: 404, url, env });
+      }
+    });
+
   });
 };
 
@@ -57,6 +58,7 @@ async function renderFragment(createApp, renderProps, initialState) {
       return false;
     }, block));
     const appDOM = app.start()({ renderProps });
+    history.push(renderProps.location.pathname);
     let html = renderToStaticMarkup(appDOM);
     return await new Promise((resolve, reject) => {
       block.wait(id, () => {
@@ -67,6 +69,7 @@ async function renderFragment(createApp, renderProps, initialState) {
     });
   } else {
     const appDOM = app.start()({ renderProps });
+    history.push(renderProps.location.pathname);
     const html = renderToStaticMarkup(appDOM);
     const curState = appDOM.props.store.getState();
     return { html, state: curState };
